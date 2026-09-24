de5e54872393b9d9dbff9e67a8dd38646ab6858f
flohms18 <trouilletflorent@outlook.com>
2026-09-24 12:16:34 +0200

    Many changes

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-def hello():
-    print("This is the forge")
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
-hello()
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+@app.get('/', response_class=HTMLResponse)
+async def home(Request):
+    return {
+        templates.TemplateResponse(
+            request=Request, name='index.html'
+        )
+    }
